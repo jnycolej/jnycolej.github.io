8ba339eda14572c3db68220f56ac82ca88d4c228
jnycolej <98425262+jnycolej@users.noreply.github.com>
2025-01-06 18:41:44 -0600

    Added functionality to website

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Jennifer Joseph</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Explore my work, projects, and skills.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a>About</a>
+        <a>Contact</a>
+        <a>Work</a>
+        <a>Resume</a>
       </header>
     </div>
   );
